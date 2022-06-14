03370f9392639fef3e0f9af520ce31fb94404409
youbbin <youbinjinyoung@gmail.com>
2022-06-14 22:12:57 +0900

    commit

---
+++ app/src/main/java/com/example/kketcontrol_youbin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kketcontrol_youbin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
